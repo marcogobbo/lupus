fe3971837a9aeb07eace695dbdde37296563c300
Marco <gobbo.marco98@gmail.com>
2020-03-11 11:26:46 +0100

    created lupus project

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var sun = new Audio('./sound/sun.mp3');
+var moon = new Audio('./sound/moon.mp3');
+var timer = 15;
+function onTimer() {
+  document.getElementById("Timer").innerHTML = timer;
+  timer--;
+  if (timer < 0) {
+    document.getElementById("Sun").style = "top: 50%;";
+    document.getElementById("Moon").style = "top: 10%;";
+    document.body.style = "background: #2C3E50;"
+    moon.play();
+  }
+  else {
+    setTimeout(onTimer, 1000);
+  }
+}
+window.onload = onTimer;
